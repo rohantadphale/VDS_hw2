29b8fc868b49ff4bb9a0d6256006efdc3bc95092
Rohan Tadphale <rohantadphale@gmail.com>
2018-09-28 17:07:54 -0500

    changes

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var svg = d3.select("svg");
+// d3.csv("https://raw.githubusercontent.com/rohantadphale/VDS_hw2/master/cities.csv", function(d) {
+//   console.log(d[0]);
+// });
 
-var path = d3.geoPath();
+// var svg = d3.select("svg");
 
-d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-  if (error) throw error;
+// var path = d3.geoPath();
 
-  svg.append("g")
-      .attr("class", "states")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-      .attr("d", path);
+// d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+//   if (error) throw error;
 
-  svg.append("path")
-      .attr("class", "state-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-});
+//   svg.append("g")
+//       .attr("class", "states")
+//     .selectAll("path")
+//     .data(topojson.feature(us, us.objects.states).features)
+//     .enter().append("path")
+//       .attr("d", path);
+
+//   svg.append("path")
+//       .attr("class", "state-borders")
+//       .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+// });
